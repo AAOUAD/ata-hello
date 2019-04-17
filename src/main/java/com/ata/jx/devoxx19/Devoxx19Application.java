45d613fa4055d955a990c5fb890f59e0aa245887
AAOUAD Toufiq <Toufiq.AAOUAD@consulting-for.accor.com>
2019-04-17 12:00:16 +0200

    Bootstrap ...

---
+++ src/main/java/com/ata/jx/devoxx19/Devoxx19Application.java
@@ -0,0 +1,13 @@
+package com.ata.jx.devoxx19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devoxx19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devoxx19Application.class, args);
+	}
+
+}
